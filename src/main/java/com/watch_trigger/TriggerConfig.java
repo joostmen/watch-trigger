9ae04b9552f0b7b502af852c4fac10ec25138892
joostmen <joost.trash@gmail.com>
2024-11-01 12:55:38 +0100

    Renamed example to Watch Trigger

--- src/main/java/com/watch_trigger/TriggerConfig.java
+++ src/main/java/com/watch_trigger/TriggerConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.watch_trigger;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("example")
-public interface ExampleConfig extends Config
+@ConfigGroup("watch_trigger")
+public interface TriggerConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/main/java/com/watch_trigger/WatchTrigger.java
+++ src/main/java/com/watch_trigger/WatchTrigger.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.watch_trigger;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,26 +14,26 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Watch Trigger"
 )
-public class ExamplePlugin extends Plugin
+public class WatchTrigger extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private TriggerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Watch Trigger started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Watch Trigger stopped!");
 	}
 
 	@Subscribe
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	TriggerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(TriggerConfig.class);
 	}
 }
